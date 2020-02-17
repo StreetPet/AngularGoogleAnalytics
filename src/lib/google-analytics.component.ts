12ef013b2ef9ac1716764a080368a0921627bbbd
Carlos Delfino - Sony 1 <consultoria@carlosdelfino.eti.br>
2020-02-17 02:31:26 -0300

    Sugestão de implementação
    Código baseado no artigo:
    https://medium.com/@PurpleGreenLemon/how-to-properly-add-google-analytics-tracking-to-your-angular-web-app-bc7750713c9e
    
    E nos manuais do Google Analytics.
    
    Verificar detalhes em Angular#2

--- src/lib/google-analytics.component.ts
+++ src/lib/google-analytics.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { GoogleAnalyticsService } from './google-analytics.service';
+
+declare let gtag: Function;
+declare let ga: Function;
 
 @Component({
-  selector: 'lib-GoogleAnalytics',
-  template: `
-    <p>
-      google-analytics works!
-    </p>
-  `,
+  selector: 'lib-google-analytics',
+  template: `<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={{id}}"></script>`,
   styles: []
 })
 export class GoogleAnalyticsComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  public id: string;
+
+  constructor(private service: GoogleAnalyticsService, private router: Router) {
+    this.service.js = new Date();
+  }
 
   ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        console.log('config');
+        (<any>window).gtag('config', this.id,
+          {
+            'page_path': event.urlAfterRedirects
+          }
+        );
+      }
+    });
   }
 
 }
--- src/lib/google-analytics.service.ts
+++ src/lib/google-analytics.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
 
+(<any>window).dataLayer = (<any>window).dataLayer || [];
+(<any>window).gtag = function(){
+  (<any>window).dataLayer.push(arguments);
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class GoogleAnalyticsService {
 
-  constructor() { }
+  set js(date:Date){
+    console.log(date);
+    (<any>window).gtag('js',date);
+  }
+  
+  public eventEmitter(
+    eventName: string,
+    eventCategory: string,
+    eventAction: string,
+    eventLabel: string = null,
+    eventValue: number = null) {
+      
+      console.log('event');
+
+      (<any>window).gtag('event', eventName, {
+      eventCategory,
+      eventLabel,
+      eventAction,
+      eventValue
+    })
+  }
 }
